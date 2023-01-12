a32df5414402509802922e27a39bd911cab6af8c
Rahul Singh <rahul.singh.ec.2017@miet.ac.in>
2023-01-12 13:18:18 +0530

    reload option enabled

--- Simple Calculator.c
+++ Simple Calculator.c
@@ -4,34 +4,42 @@
 void main(){
 	float a,b,c;
 	int choice;
+	char l;
 	printf("\t\tSimple Calculator");
 	printf("\nEnter first number:");
 	scanf("%f",&a);
 	printf("\nEnter second number:");
 	scanf("%f",&b);
-	printf("\n\nEnter your choice: \n\t1.Addition \n\t2.Substraction \n\t3.Multiplication \n\t4.Division \n");
-	scanf("%d",&choice);
-	switch(choice){
-		case 1:
-			c = a+b;
-			printf("Addition: %0.2f",c);
-			break;
-		case 2:
-			c = a-b;
-			printf("Substraction: %0.2f",c);
-			break;
-		case 3:
-			c = a*b;
-			printf("Multiplication: %0.2f",c);
-			break;
-		case 4:
-			c = a/b;
-			printf("Division: %0.2f",c);
-			break;
-		default:
-			printf("you have entered wrong choice");
-			
+	
+	do{
+		printf("\n\nEnter your choice: \n\t1.Addition \n\t2.Substraction \n\t3.Multiplication \n\t4.Division \n");
+		scanf("%d",&choice);
+		switch(choice){
+			case 1:
+				c = a+b;
+				printf("Addition: %0.2f",c);
+				break;
+			case 2:
+				c = a-b;
+				printf("Substraction: %0.2f",c);
+				break;
+			case 3:
+				c = a*b;
+				printf("Multiplication: %0.2f",c);
+				break;
+			case 4:
+				c = a/b;
+				printf("Division: %0.2f",c);
+				break;
+			default:
+				printf("you have entered wrong choice");
 	}
+	printf("\nDo you want to continue(y/n):");
+	l = getch();
+	}
+	while(l == 'y');
+
+	
 	
 	getch();
 }
